2a92fc31f2841ab36c28ba252b62f9fc0e84009f
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-23 23:58:15 +0600

    Basic Setup Done for this Project

--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <div>
-        <h2>Career Craft Project Setup Done</h2>
-<Button>Click me</Button>
-        {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
+    <div>
+      <h2>Career Craft Project Setup Done</h2>
+      <Button>Click me</Button>
+      {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             target="_blank"
@@ -22,6 +22,6 @@ export default function Home() {
             Read our docs
           </a>
         </div> */}
-      </div>
+    </div>
   );
 }
