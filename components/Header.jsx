fb362bb4fab6b1a6b7d03310a412dade056202e9
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-27 22:39:14 +0600

    Header Component Updated

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div>
       <p className="text-xl font-semibold text-blue-500">Header is here</p>
-      <p>New Paragraph in the town</p>
+      <p className="text-xl font-semibold">New Paragraph in the town</p>
     </div>
   );
 };
