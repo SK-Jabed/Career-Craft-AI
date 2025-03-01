4f81eb5048168d63e2b2e40688eb77d215a2c23d
Sheikh Jabed <jabedbd2295@gmail.com>
2025-03-01 14:44:39 +0600

    Navbar Added and Authencication Activities Done

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,40 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { Link } from "lucide-react";
+import Image from "next/image";
 import React from "react";
+import { Button } from "./ui/button";
 
 const Header = () => {
   return (
-    <div>
-      <p className="text-xl font-semibold text-blue-500">Header is here</p>
-      <p className="text-xl font-semibold">New Paragraph in the town</p>
-    </div>
+    <header>
+      <nav>
+
+      <Link href="/">
+          <Image
+            src={"/logo.png"}
+            alt="CareerCraft Logo"
+            width={200}
+            height={60}
+            className="h-12 py-1 w-auto object-contain"
+          />
+        </Link>
+
+        <div>
+          <SignedIn>
+            <Link>
+            <Button></Button>
+            </Link>
+          </SignedIn>
+        </div>
+      </nav>
+                  <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+    </header>
   );
 };
 
