349036c962d1932a52dca5b9061f77e54ac5246c
Sheikh Jabed <jabedbd2295@gmail.com>
2025-03-06 23:37:13 +0600

    Footer Section's Design Updated

--- app/layout.js
+++ app/layout.js
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
             <main className="min-h-screen">{children}</main>
             {/* Footer */}
             <footer className="bg-muted/50 py-12">
-              <div className="container mx-auto px-4 text-center text-gray-200">
+              <div className="container mx-auto px-6 text-center text-gray-200">
                 <p>Created By Sheikh Jabed</p>
               </div>
             </footer>
