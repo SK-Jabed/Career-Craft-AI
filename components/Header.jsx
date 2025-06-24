762b3eb6c0c276922185d9523f2942cee0046a68
Sheikh Jabed <jabedbd2295@gmail.com>
2025-06-24 23:42:14 +0600

    [Done]: Activities and functionalities related to protect routes done

--- components/Header.jsx
+++ components/Header.jsx
@@ -24,10 +24,6 @@ import {
   StarsIcon,
 } from "lucide-react";
 
-
-
-
-
 const Header = () => {
   return (
     <header className="fixed top-0 w-full border-b-2 bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
